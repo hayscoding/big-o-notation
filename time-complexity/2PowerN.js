08ee31b08c009bd02d2d0d169fd544cddd8b044e
Hays Stanford <stanfordhays@gmail.com>
2020-02-15 20:54:50 -0600

    added example for 2PowerN.js

--- time-complexity/2PowerN.js
+++ time-complexity/2PowerN.js
@@ -3,3 +3,16 @@
     that solve a problem of size N by recursively solving two smaller 
     problems of size N-1.
 */
+
+
+// Finds num at nth position in the fibonacci sequence 
+// (1, 1, 2, 3, 5, 8, 13, 21, 34, ...)
+function fibonacci(n) {
+    if (n < 2) {
+        return n;
+    }
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+console.log(fibonacci(8))
