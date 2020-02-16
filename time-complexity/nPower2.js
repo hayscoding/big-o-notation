6c2f288bd575236abfd48360574b19c08fff5755
Hays Stanford <stanfordhays@gmail.com>
2020-02-15 20:24:30 -0600

    added comment to nPower2.js

--- time-complexity/nPower2.js
+++ time-complexity/nPower2.js
@@ -1,5 +1,6 @@
 function printAllOrderedPairs(arr) {
     for (let x = 0; x < arr.length; x++) {
+        // Iterates thru array for each index in the array (hence n^2)
         for (let y = 0; y < arr.length; y++) {
             console.log(`[${arr[x]}, ${arr[y]}]`)
         }
