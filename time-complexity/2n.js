61948e14ebd0bf62a55a2e012ab509af9823b91e
Hays Stanford <stanfordhays@gmail.com>
2020-02-15 20:25:33 -0600

    added example for 2n.js

--- time-complexity/2n.js
+++ time-complexity/2n.js
@@ -1,3 +1,13 @@
-function printAllOrderedPairs(arr) {
+function printIndicesTwice(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        console.log(arr[i])
+    }
 
+    for (var i = 0; i < arr.length; i++) {
+        console.log(arr[i])
+    }
 }
+
+var testArr = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+printIndicesTwice(testArr)
