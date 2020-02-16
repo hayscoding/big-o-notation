cb0c88eb7d02648925431bcbba0e7209582adccb
Hays Stanford <stanfordhays@gmail.com>
2020-02-15 20:24:20 -0600

    added example for nPower2.js time complexity

---
+++ time-complexity/2n.js
@@ -0,0 +1,3 @@
+function printAllOrderedPairs(arr) {
+
+}
---
+++ time-complexity/nPower2.js
@@ -0,0 +1,11 @@
+function printAllOrderedPairs(arr) {
+    for (let x = 0; x < arr.length; x++) {
+        for (let y = 0; y < arr.length; y++) {
+            console.log(`[${arr[x]}, ${arr[y]}]`)
+        }
+    }
+}
+
+var testArr = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+printAllOrderedPairs(testArr)
